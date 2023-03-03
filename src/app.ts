90922af4dceb35f8d12902c7e07e36e114e6a8af
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-03 10:44:22 -0300

    feat: all routers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
-
+import categoriesRoutes from "./routers/categories/categories.router";
+import loginRoutes from "./routers/login/login.router";
+import realEstateRoutes from "./routers/realEstate/realEstate.router";
+import schedulesRoutes from "./routers/schedules/schedules.router";
+import usersRoutes from "./routers/users/users.router";
 
 const app: Application = express();
 app.use(express.json());
 
-// app.use();
+app.use("/users", usersRoutes);
+app.use("/login", loginRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/realEstate", realEstateRoutes);
+app.use("/schedules", schedulesRoutes);
 
 app.use(handleErrors);
 
