7788607d8ca293fe05ba94758d0a33630df751d7
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-03 10:49:00 -0300

    feat: index.ts entities

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import { Address } from './<arquivo>';
+import { Category } from './<arquivo>';
+import { RealEstate } from './<arquivo>';
+import { Schedule } from './<arquivo>';
+import { User } from './<arquivo>';
+
+export { Address, RealEstate, Category, User, Schedule };
