411c50867577af3fa7accd2ece4965f2a62d7ae8
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-03 10:29:30 -0300

    feat: dependencies

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application } from "express";
+import { handleErrors } from "./errors";
+
+
+const app: Application = express();
+app.use(express.json());
+
+// app.use();
+
+app.use(handleErrors);
+
+export default app;
