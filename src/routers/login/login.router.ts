b0e0d8527f5fe60b43266dadadd005fa1fa346ae
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-05 19:14:02 -0300

    feat: route login

--- src/routers/login/login.router.ts
+++ src/routers/login/login.router.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { createLoginController } from "../../controllers/login.controller";
+// import ensureTokenIsValidMiddleware from "../../services/users/ensureTokenIsValid.middleware";
 
 const loginRoutes: Router = Router();
 
-loginRoutes.post("");
+loginRoutes.post("", createLoginController);
 
 export default loginRoutes;
---
+++ src/services/login/createLogin.service.ts
@@ -0,0 +1,42 @@
+import { compare } from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { AppDataSource } from "../../data-source";
+import { User } from "../../entities";
+import { AppError } from "../../errors";
+import "dotenv/config";
+import { Repository } from "typeorm";
+import { ILogin } from "../../interfaces/login.interface";
+
+const createLoginService = async (loginData: ILogin): Promise<string> => {
+  const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
+  const user: User | null = await userRepository.findOneBy({
+    email: loginData.email,
+  });
+
+  if (!user) {
+    throw new AppError("Wrong email or password", 401);
+  }
+
+  const passwordMatch = await compare(loginData.password, user.password);
+
+  if (!passwordMatch) {
+    throw new AppError("Wrong email or password", 401);
+  }
+
+  const token: string = jwt.sign(
+    {
+      admin: user.admin,
+      id: user.id,
+    },
+    process.env.SECRET_KEY!,
+    {
+      expiresIn: process.env.EXPIRES_IN!,
+      subject: String(user.id),
+    }
+  );
+
+  return token;
+};
+
+export default createLoginService;
